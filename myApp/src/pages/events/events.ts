e76a678051f8eea87f68bfb0c293dae39a02d539
manav <manav@mail.uoguelph.ca>
2017-02-05 03:21:24 -0500

    manavs commit

--- myApp/src/pages/events/events.ts
+++ myApp/src/pages/events/events.ts
@@ -14,9 +14,21 @@ import { NavController, NavParams } from 'ionic-angular';
 export class EventsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  items = [
+    'UofT Hacks',
+    'HackCU III',
+    'enghacks',
+    'hackNY',
+    'CarlHacks'
+  ];
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventsPage');
   }
-
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
+   infoPage(r) {
+  	this.navCtrl.push(InfoPage);
+  }
 }

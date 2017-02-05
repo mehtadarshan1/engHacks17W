9a8d83dfd1a3f8f64f1f792327b970fc08df6351
mehtadarshan <darshanmehta786@yahoo.in>
2017-02-04 23:43:23 -0500

    added button

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {EventsPage} from '../pages/events/events';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EventsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EventsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {EventsPage} from '../events/events';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
     
+  }
+
+  eventsPage(r) {
+  	this.navCtrl.push(EventsPage);
   }
 
 }
